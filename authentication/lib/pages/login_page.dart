a9d0e1127df1fc922a2eb442668c1b095376092a
B.Larissa <109439888+ZigaLarissa@users.noreply.github.com>
2024-02-25 17:41:12 +0200

    a textfield component to be reused

--- authentication/lib/pages/login_page.dart
+++ authentication/lib/pages/login_page.dart
@@ -1,12 +1,58 @@
 // ignore_for_file: prefer_const_constructors
 
 import "package:flutter/material.dart";
+import "/components/my_textfield.dart";
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        backgroundColor: Colors.grey[300],
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(height: 50),
+
+                // logo icon
+                Icon(
+                  Icons.lock,
+                  size: 100,
+                ),
+
+                // welcome back!
+                SizedBox(height: 20),
+
+                Text(
+                  "Welcome back, we missed you!",
+                  style: TextStyle(color: Colors.grey[700], fontSize: 16),
+                ),
+
+                // username textfield
+                SizedBox(height: 20),
+
+                MyTextField(),
+
+                SizedBox(height: 10),
+
+                MyTextField(),
+
+                // password textfield
+
+                // forgot password?
+
+                // login button
+
+                // or continue with
+
+                // google and mac buttons
+
+                // not a member? sign up
+              ],
+            ),
+          ),
+        ));
   }
 }
